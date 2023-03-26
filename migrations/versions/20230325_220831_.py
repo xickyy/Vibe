fd5c0a0b15508bfdc16b434c88fb2b80eb1a4848
ricky <ricky_moore@ymail.com>
2023-03-26 12:05:01 -0700

    added environment variables for render yet again lol

--- migrations/versions/20230325_220831_.py
+++ migrations/versions/20230325_220831_.py
@@ -1,12 +1,16 @@
 """empty message
 
 Revision ID: 6a8ec1e6b22a
-Revises: 
+Revises:
 Create Date: 2023-03-25 22:08:31.150318
 
 """
 from alembic import op
 import sqlalchemy as sa
+
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 
 # revision identifiers, used by Alembic.
@@ -38,6 +42,9 @@ def upgrade():
     sa.UniqueConstraint('email'),
     sa.UniqueConstraint('username')
     )
+    if environment == "production":
+        op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
+
     op.create_table('booleans',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('first_name_b', sa.Boolean(), nullable=True),
@@ -54,6 +61,9 @@ def upgrade():
     sa.ForeignKeyConstraint(['user_id'], ['users.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
+    if environment == "production":
+        op.execute(f"ALTER TABLE booleans SET SCHEMA {SCHEMA};")
+
     op.create_table('friends',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('friend_rank', sa.Integer(), nullable=True),
@@ -63,6 +73,9 @@ def upgrade():
     sa.ForeignKeyConstraint(['user_id'], ['users.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
+    if environment == "production":
+        op.execute(f"ALTER TABLE friends SET SCHEMA {SCHEMA};")
+
     op.create_table('posts',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('body', sa.String(length=255), nullable=False),
@@ -72,6 +85,9 @@ def upgrade():
     sa.ForeignKeyConstraint(['user_id'], ['users.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
+    if environment == "production":
+        op.execute(f"ALTER TABLE posts SET SCHEMA {SCHEMA};")
+
     # ### end Alembic commands ###
 
 
