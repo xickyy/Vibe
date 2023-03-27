bb8990c000bc5ae5258adb7e31c063afc4d3cd74
ricky <ricky_moore@ymail.com>
2023-03-26 18:46:07 -0700

    friend pages how their details now

--- react-app/src/components/FriendProfilePage/index.js
+++ react-app/src/components/FriendProfilePage/index.js
@@ -58,24 +58,172 @@ const FriendProfilePage = () => {
     }
     if (!Object.values(friendState).find(friend => friend.friendId === parseInt(friendId))) {
       return (
-        <button onClick={() => handleAddFriend()}>Follow this user</button>
+        <button className="friend-follow-button" onClick={() => handleAddFriend()}>Follow this user</button>
       )
     } else {
       return (
-        <button onClick={() => handleDeleteFriend()}>Unfollow this user</button>
+        <button className="friend-follow-button" onClick={() => handleDeleteFriend()}>Unfollow this user</button>
       )
     }
   }
 
-  return (
-    isLoaded &&
-    <div style={{ position: 'absolute', marginTop: '-56px', zIndex: '-100', height: '99%', width: '99%', backgroundImage: `url(${profile.profileBackgroundImgUrl})` }}>
-      <div style={{ marginTop: '70px' }}>
-        <h1>friend-{profile.firstName}</h1>
-        {friendButton()}
+
+
+
+
+  const ifBackground = () => {
+    if ((isLoaded && (profile.profileBackgroundImgUrl !== '')) && (profile.booleans.backgroundB)) {
+      return {
+        position: 'absolute',
+        marginTop: '-57px',
+        marginLeft: '-8px',
+        zIndex: '-100',
+        height: '100%',
+        width: '100%',
+        backgroundImage: `url(${profile.profileBackgroundImgUrl})`
+      }
+    }
+  }
+
+  const ifNoBackground = () => {
+    if (((isLoaded && (profile.profileBackgroundImgUrl === '')) || (isLoaded && !profile.booleans.backgroundB))) {
+      return { marginTop: '15px' }
+    } else {
+      return { marginTop: '70px' }
+    }
+  }
+
+  const ifFirstName = () => {
+    if ((isLoaded && (profile.firstName !== '')) && (profile.booleans.firstNameB)) {
+      return (
+        <div className="user-profile-first-name">{profile.firstName}</div>
+      )
+    }
+  }
+
+  const ifLastName = () => {
+    if ((isLoaded && (profile.lastName !== '')) && (profile.booleans.lastNameB)) {
+      return (
+        <div className="friend-last-name">{profile.lastName}</div>
+      )
+    }
+  }
+
+  const ifCard = () => {
+    if ((isLoaded && (profile.cardImgUrl !== '')) && (profile.booleans.cardB)) {
+      return (
+        <img className="user-profile-card-img" src={profile.cardImgUrl} alt=''></img>
+      )
+    }
+  }
+
+  const ifMotto = () => {
+    if ((isLoaded && (profile.motto !== '')) && (profile.booleans.mottoB)) {
+      return (
+        <div className="user-profile-motto">{profile.motto}</div>
+      )
+    } else {
+      return (
+        <div className="user-profile-motto"></div>
+      )
+    }
+  }
+
+  const ifRelationship = () => {
+    if ((isLoaded && profile.booleans.relationshipB)) {
+      return (
+        <div className="user-profile-detail">Relationship: {profile.relationshipStatus}</div>
+      )
+    }
+  }
+
+  const ifBirthday = () => {
+    if ((isLoaded && (profile.birthday !== '')) && (profile.booleans.birthdayB)) {
+      return (
+        <div className="user-profile-detail">Birthday: {profile.birthday}</div>
+      )
+    }
+  }
+
+  const ifZodiac = () => {
+    if (isLoaded  && profile.booleans.zodiacB) {
+      return (
+        <div className="user-profile-detail">Zodiac: {profile.zodiac}</div>
+      )
+    }
+  }
+
+  const ifHeight = () => {
+    if ((isLoaded && (profile.height !== '')) && (profile.booleans.heightB)) {
+      return (
+        <div className="user-profile-detail">Height: {profile.height}</div>
+      )
+    }
+  }
+
+  const ifBio = () => {
+    if ((isLoaded && (profile.bio !== '')) && (profile.booleans.bioB)) {
+      return (
+        <div className="user-profile-bio">Bio: {profile.bio}</div>
+      )
+    }
+  }
+
+  const ifBioCss = () => {
+    if ((isLoaded && (profile.bio !== '')) && (profile.booleans.bioB)) {
+      return "user-bio-container"
+    }
+  }
+
+  const ifDetailsCss = () => {
+    if (isLoaded && (profile.booleans.zodiacB || profile.booleans.heightB || profile.booleans.relationshipB || profile.booleans.birthdayB)) {
+      return "user-profile-details"
+    }
+  }
+
+
+
+
+
+  return ( isLoaded &&
+    <div style={ifBackground()}>
+      <div style={ifNoBackground()}>
+        <div className="user-profile-header">
+          <div className="user-profile-card-details">
+            {profile.username}
+            {ifMotto()}
+            {ifCard()}
+          </div>
+          <div className="user-profile-first-last-name">
+            {ifFirstName()}
+            {ifLastName()}
+            {friendButton()}
+          </div>
+        </div>
+        <div className="user-profile-bio-and-details">
+          <div className={ifDetailsCss()}>
+            {ifRelationship()}
+            {ifBirthday()}
+            {ifZodiac()}
+            {ifHeight()}
+          </div>
+          <div className={ifBioCss()}>
+            {ifBio()}
+          </div>
+        </div>
       </div>
     </div>
   )
+
+  // return (
+  //   isLoaded &&
+  //   <div style={{ position: 'absolute', marginTop: '-56px', zIndex: '-100', height: '99%', width: '99%', backgroundImage: `url(${profile.profileBackgroundImgUrl})` }}>
+  //     <div style={{ marginTop: '70px' }}>
+  //       <h1>friend-{profile.firstName}</h1>
+  //       {friendButton()}
+  //     </div>
+  //   </div>
+  // )
 
 }
 
