490da7c0e36f151908ecbd0142c63300473a8094
ricky <ricky_moore@ymail.com>
2023-04-10 19:18:50 -0700

    fixed a few warnings for my about links'

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="footer">
       <u>Made by:</u>
       <div>
-      Ricky Moore <a href="https://www.linkedin.com/in/ricky-moore-b629b21ba/" target="_blank"><i class="fa-brands fa-linkedin-in"></i></a> | <a href="https://github.com/xickyy" target="_blank"><i class="fa-brands fa-github"></i></a>
+      Ricky Moore <a href="https://www.linkedin.com/in/ricky-moore-b629b21ba/" target="_blank" rel="noreferrer"><i class="fa-brands fa-linkedin-in"></i></a> | <a href="https://github.com/xickyy" target="_blank" rel="noreferrer"><i class="fa-brands fa-github"></i></a>
       </div>
     </footer>
   )
